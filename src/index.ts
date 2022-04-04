acf99ef085b9d86e73c51efc93c533b98edfdc47
Andrew G. Knackstedt <andrew.knackstedt@dynatrace.com>
2022-04-04 10:11:00 -0400

    Add groups to the regex

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@
  * Assert if a string is a Dynatras SaaS url, Managed URL, or ActiveGate URL (incl. localhost).
  *  - https://abc012345.live.dynatrace.com/
  */
-export const dynatraceUrlRegex = /^(https?:\/\/)?([a-z]{3}[0-9]{5}\.(sprint|live|dev)\.(dynatrace|dynatracelabs)\.com|.+?\/e\/[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}|(127\.0\.0\.1|localhost|\[::1\]|.+?):9999\/e\/([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}|[a-z]{3}\d{5}))\/?$/;
+export const dynatraceUrlRegex = /^(?<protocol>https?:\/\/)?(?<tenantId>[a-z]{3}[0-9]{5}\.(?<saasStage>sprint|live|dev)\.(?<saasDomain>dynatrace|dynatracelabs)\.com|.+?\/e\/(?<tenantId>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})|(127\.0\.0\.1|localhost|\[::1\]|.+?):9999\/e\/(?<tenantId>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}|[a-z]{3}\d{5}))\/?$/;
 
 /**
  * Assert if a string is a Dynatrace SaaS url.
@@ -18,7 +18,7 @@ export const dynatraceUrlRegex = /^(https?:\/\/)?([a-z]{3}[0-9]{5}\.(sprint|live
  * | 3 | Stage  |
  * | 4 | Domain |
  */
-export const dynatraceSaaSUrlRegex = /^(https?:\/\/)([a-z]{3}[0-9]{5})\.(sprint|live|dev)\.(dynatrace|dynatracelabs)\.com\/?$/;
+export const dynatraceSaaSUrlRegex = /^(?<protocol>https?:\/\/)(?<tenantId>[[a-z]{3}[0-9]{5})\.(?<saasStage>sprint|live|dev)\.(?<saasDomain>dynatrace|dynatracelabs)\.com\/?$/;
 
 /**
  * Assert if a string is a Dynatrace SaaS host.
@@ -31,7 +31,7 @@ export const dynatraceSaaSUrlRegex = /^(https?:\/\/)([a-z]{3}[0-9]{5})\.(sprint|
  * | 2 | Stage  |
  * | 3 | Domain |
  */
-export const dynatraceSaaSHostRegex = /^([a-z]{3}[0-9]{5})\.(sprint|live|dev)\.(dynatrace|dynatracelabs)\.com\/?$/;
+export const dynatraceSaaSHostRegex = /^(?<tenantId>[a-z]{3}[0-9]{5})\.(?<saasStage>sprint|live|dev)\.(?<saasDomain>dynatrace|dynatracelabs)\.com\/?$/;
 
 /**
  * Assert if a string is either a V1 or V2 API token format.
@@ -43,7 +43,7 @@ export const dynatraceSaaSHostRegex = /^([a-z]{3}[0-9]{5})\.(sprint|live|dev)\.(
  * | 2 | Public Portion of token | This is a 24-character public identifier of the token. This value can be safely displayed in the UI and can be used for logging purposes. |
  * | 3 | Secret Portion of token | This is a 64-character secret portion of the token, which can be treated like a password and therefore doesn’t need to be displayed in the Dynatrace web UI (following initial creation) or stored in log files. |
  */
-export const dynatraceTokenRegex = /^[A-Za-z0-9-_]{21}$|^(dt\d\w\d{2})\.([A-Z\d]{24})\.([A-Z\d]{64})$/;
+export const dynatraceTokenRegex = /^(?<v1Token>[A-Za-z0-9-_]{21})$|^(?<v2Prefix>dt\d\w\d{2})\.(?<v2Public>[A-Z\d]{24})\.(?<v2Private>[A-Z\d]{64})$/;
 
 /**
  * Assert if a string matches the Dynatrace V1 Token format.
@@ -65,5 +65,5 @@ export const dynatraceV1TokenFormatRegex = /^[a-zA-Z0-9\-_]{21}$/;
  * | 2 | Public Portion of token | This is a 24-character public identifier of the token. This value can be safely displayed in the UI and can be used for logging purposes. |
  * | 3 | Secret Portion of token | This is a 64-character secret portion of the token, which can be treated like a password and therefore doesn’t need to be displayed in the Dynatrace web UI (following initial creation) or stored in log files. |
  */
-export const dynatraceV2TokenFormatRegex = /^(dt\d\w\d{2})\.([A-Z\d]{24})\.([A-Z\d]{64})$/;
+export const dynatraceV2TokenFormatRegex = /^(?<v2Prefix>dt\d\w\d{2})\.(?<v2Public>[A-Z\d]{24})\.(?<v2Private>[A-Z\d]{64})$/;
 
